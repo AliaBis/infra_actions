c188ad0b1c0844bea81a5e52e3013dc456f3c356
Алия Бисенгалиева <105454189+AliaBis@users.noreply.github.com>
2022-11-22 23:02:30 +0300

    !

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 
 from . import views
@@ -7,6 +7,6 @@ app_name = 'infra_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('second/', views.second_page, name='second_page'),
-    path('admin/', admin.site.urls),
+    path('second_page/', views.second_page, name='second_page'),  # добавлено _page
+    #path('admin/', admin.site.urls),
 ]
