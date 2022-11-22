f314292a3f1c57bacb40e599586e3f3e732b21e9
Алия Бисенгалиева <105454189+AliaBis@users.noreply.github.com>
2022-11-22 19:42:51 +0300

    исправление пути ко 2 странице

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib import admin # добавлено
 from . import views
 
 app_name = 'infra_app'
@@ -7,5 +7,5 @@ app_name = 'infra_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('second/', views.second_page, name='second_page'),
-
+    path('admin/', admin.site.urls), # добавлено
 ]
